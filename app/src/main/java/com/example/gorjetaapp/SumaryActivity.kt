dd73723035c95bb44ee0cca7f736637d82035d22
GiovaneDev <giovane.santosdm@gmail.com>
2024-10-15 23:04:39 +0100

    summary screen layout

--- app/src/main/java/com/example/gorjetaapp/SumaryActivity.kt
+++ app/src/main/java/com/example/gorjetaapp/SumaryActivity.kt
@@ -5,16 +5,29 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.gorjetaapp.databinding.ActivitySumaryBinding
 
 class SumaryActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySumaryBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sumary)
+        binding = ActivitySumaryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val totalTable = intent.getFloatExtra("totalTable", 0.0f)
         val numPeople = intent.getIntExtra("numpeople", 0)
         val percentage = intent.getIntExtra("percentage", 0)
         val totalAmount = intent.getFloatExtra("totalAmount", 0.0f)
+
+        binding.tvTotalTable.text = totalTable.toString()
+        binding.tvNumPeople.text = numPeople.toString()
+        binding.tvPercentage.text = percentage.toString()
+        binding.tvTotalAmount.text = totalAmount.toString()
+
+        binding.btnRefresh.setOnClickListener {
+            finish()
+        }
 
 
 
