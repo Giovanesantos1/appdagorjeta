bfe36fff241ede670941087ce2634f0bd9d0f9c4
GiovaneDev <giovane.santosdm@gmail.com>
2024-10-15 20:48:09 +0100

    limpando valores

--- app/src/main/java/com/example/gorjetaapp/MainActivity.kt
+++ app/src/main/java/com/example/gorjetaapp/MainActivity.kt
@@ -58,6 +58,15 @@ class MainActivity : AppCompatActivity() {
                 binding.tvResult.text = "Total width tips: $totalWithTips"
             }
 
+            binding.btnClean.setOnClickListener {
+                binding.tvResult.text = ""
+                binding.tilTotal.setText("")
+                binding.numberOfPeople.setText("")
+                binding.rbOptionOne.isChecked = false
+                binding.rbOptionTwo.isChecked = false
+                binding.rbOptionThree.isChecked = false
+            }
+
         }
 
 
